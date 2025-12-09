a6a20170ccb8fb68d403b084f9615d81f350c673
Magne Rayan <rayan.magne@supdevinci-edu.fr>
2025-12-09 16:01:14 +0100

    feat : init routes /, /game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
 
 import HomePage from "./Pages/home"
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import GamePage from "./Pages/game"
 
 
 function App() {
-
   return (
     <>
-      <div>
-        <p>test</p>
-      </div>
-      <HomePage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </Router>
     </>
   )
 }
 
-export default App
+export default App;
